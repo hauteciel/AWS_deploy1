f0593163d25a4f0fe56f7b526a714e83d4c4fd16
hauteciel <hauteciel@gmail.com>
2025-05-07 10:04:26 +0900

    init

---
+++ src/main/java/com/lec/spring/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.lec.spring.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HomeController {
+
+    @GetMapping("/aws/v1")
+    public String hello(@RequestParam(defaultValue = "1") Integer num){
+        if(num == 1) {
+            log.info("💚/aws/v1 호출. info로그 ");
+        } else if(num == -1){
+            log.error("💥/aws/v1 호출. error로그 ");
+        } else if(num == 0){
+            log.warn("💛/aws/v1 호출. warning로그 ");
+        }
+
+        return "<h1>AWS 배포 v1</h1>";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
